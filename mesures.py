7653487ec26402695f54d3351bf34af26af59b46
hugo <hugoconstantin78@gmail.com>
2026-06-26 17:17:02 +0200

    mesures into csv

--- mesures.py
+++ mesures.py
@@ -1,1 +1,64 @@
-import math
+import csv, smbus2, time, adafruit_dht, adafruit_blinka, board
+
+adress = 0x40
+bus = smbus2.SMBus(1)
+capteur = adafruit_dht.DHT22(board.D12)
+
+
+def capt_air():
+    try:
+        bus.write_byte(adress, 0x88)
+        time.sleep(0.3)
+        data = bus.read_i2c_block_data(adress, 0x00, 29)
+
+        pm1_atm = (data[10] << 8) | data[11]
+        pm25_atm = (data[12] << 8) | data[13]
+        pm10_atm = (data[14] << 8) | data[15]
+
+        return {
+            "pm1_atm": pm1_atm,
+            "pm25_atm": pm25_atm,
+            "pm10_atm": pm10_atm,
+            "unite": "µg/m3",
+        }
+    
+    except OSError:
+        return {
+            "pm1_atm": "Erreur!",
+            "pm25_atm": "Erreur!",
+            "pm10_atm": "Erreur!",
+            "unite": "",
+        }
+
+def temp_hum():
+
+    try:
+        temperature = capt_temp.temperature
+        humidite = capt_temp.humidity
+
+        return {
+            "temperature": temperature, 
+            "humidite": humidite, 
+            "unite_temp": "°C", 
+            "unite_hum": "%",
+        }
+
+    except RuntimeError:
+        return {
+            "temperature": "Erreur!", 
+            "humidite": "Erreur!", 
+            "unite_temp": "", 
+            "unite_hum": ""
+        }
+
+valeur_air = capt_air()
+val_temp_hum = temp_hum()
+
+
+with open("mesures.csv", mode="a", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow({val_temp_hum["temperature"]},
+                    {val_temp_hum["humidite"]}, 
+                    {valeur_air['pm1_atm']}, 
+                    {valeur_air['pm25_atm']}, 
+                    {valeur_air['pm10_atm']})
