68a7f06d58cef926e452fc70305210860de458a8
Julien <julien.marconcini@pm.me>
2026-07-02 10:59:23 +0200

    Importation de la bibliothèque rover

--- mouvements.py
+++ mouvements.py
@@ -1,4 +1,10 @@
-import RPi.GPIO as GPIO, sys, time, os
-from rpi_ws281x import *
-import pca9685
-import smbus
+import sys, os, time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "backups", "rover"))
+
+try:
+    import rover
+    print("[mouvement.py] rover.py chargé (Raspberry Pi Zero)")
+except RuntimeError:
+    import fakeRover as rover
+    print("[mouvements.py] fakeRover.py chargé (PC)")
