451ddcfb3bd0560d884259cd38cc92e45f7c2576
Julien <julien.marconcini@pm.me>
2026-07-02 12:54:04 +0200

    Correction import rover.py/fakerover.py

--- mouvements.py
+++ mouvements.py
@@ -1,6 +1,7 @@
 import sys, os, time
-from mesures import ecriture
+#from mesures import ecriture
 
+sys.path.append(os.path.join(os.path.dirname(__file__), "backups", "capteurs"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "backups", "rover"))
 
 try:
