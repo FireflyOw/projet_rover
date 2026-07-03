175ca9fdecfbec65b64db236016ee69f0a170644
hugo <hugoconstantin78@gmail.com>
2026-07-03 15:54:58 +0200

    test mvt

--- backups/capteurs/test_mvt.py
+++ backups/capteurs/test_mvt.py
@@ -69,9 +69,9 @@ rover.init(0)
 init_servo()
 time.sleep(2)
 
-rover.setServo(servo_Sonar, -95)
+rover.setServo(servo_Sonar, 95)
 time.sleep(1)
-rover.setServo(servo_Sonar, 90)
+rover.setServo(servo_Sonar, -30)
 time.sleep(1)
 
 print("bloblo")
