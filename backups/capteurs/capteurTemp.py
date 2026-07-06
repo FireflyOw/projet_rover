713574852fccc4c14cafdf80278e82c69c379ad0
Julien <julien.marconcini@pm.me>
2026-07-06 13:41:41 +0200

    Ajout boucle while

--- backups/capteurs/capteurTemp.py
+++ backups/capteurs/capteurTemp.py
@@ -1,16 +1,17 @@
-import time, adafruit_dht, adafruit_blinka, board
+import time, adafruit_dht, board
 
 capteur = adafruit_dht.DHT22(board.D25)
+try:
+    while True:
+        try:
+            temperature = capteur.temperature
+            humidite = capteur.humidity
 
-while True:
-    try:
-        temperature = capteur.temperature
-        humidite = capteur.humidity
+            print(f"Température : {temperature:.1f}°C")
+            print(f"Humidité : {humidite:.1f}%")
 
-        print(f"Température : {temperature:.1f}°C")
-        print(f"Humidité : {humidite:.1f}%")
-
-    except RuntimeError as e:
-        print(f"Erreur de lecture {e}")
-    finally:
-        capteur.exit()
+        except RuntimeError as e:
+            print(f"Erreur de lecture {e}")
+            
+finally:
+    capteur.exit()
