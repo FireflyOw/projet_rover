199581a32240f6598c03e0c232368a391d379056
hugo <hugoconstantin78@gmail.com>
2026-07-07 13:48:27 +0200

    test mvt

--- backups/capteurs/test_mvt.py
+++ backups/capteurs/test_mvt.py
@@ -86,7 +86,7 @@ try:
        print(distance[-1:])
        goForward(speed)
        if all(x<=30 for x in distance[-5:]):
-           while distance[-1:]<=30:
+           while (x<=30 for x in distance[-1:]):
                 distance.append(rover.getDistance())
                 rover.spinLeft(70)
                 print("bloblo")
