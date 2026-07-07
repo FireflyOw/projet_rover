0ef1c610b91fcaf4fb2eaf4092c716aca5468b36
Julien <julien.marconcini@pm.me>
2026-07-07 15:20:49 +0200

    test mvt

--- backups/capteurs/test_mvt.py
+++ backups/capteurs/test_mvt.py
@@ -91,7 +91,7 @@ try:
             Spin=False
 
         if all(x<=30 for x in distance[-5:]):
-           while all(x<=30 for x in distance[-1000:]):
+           while all(x<=30 for x in distance[-500:]):
                 distance.append(rover.getDistance())
                 time.sleep(0.001)
                 print(distance[-1:])
@@ -100,6 +100,9 @@ try:
                     Spin=True
                     Av=False
                 print("bloblo")
+
+        time.sleep(0.001)
+
     
 finally: 
    rover.cleanup()
