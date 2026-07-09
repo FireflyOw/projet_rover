14228e085688aa6a86ecadea7f741faa29743bf0
Julien <julien.marconcini@pm.me>
2026-07-09 12:36:59 +0200

    Fix: erreurs route /history

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-import csv
+import csv, os, time
 from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
-CSV_PATH = "mesures.csv"
+def getCSVpath():
+    return os.path.join(os.path.dirname(__file__), "données", time.strftime("mesures%b%d.csv"))
 
 def readCSV():
     try:
-        with open (CSV_PATH, "r", newline="") as f:
+        with open (getCSVpath(), "r", newline="") as f:
             reader = csv.DictReader(f)
             rows = list(reader)
         return rows
@@ -18,18 +19,18 @@ def readCSV():
 def index():
     return render_template("index.html")
 
-@app.route("/api/sensor/latest")
+@app.route("/api/sensors/latest")
 def latest():
     rows = readCSV()
     if not rows:
         return jsonify({"error": "Aucune donnée disponible!"})
     
-    return jsonify(rows[-1:])
+    return jsonify(rows[-1])
 
-@app.route("/api/sensor/historque")
-def historque():
+@app.route("/api/sensors/history/<int:count>")
+def historque(count):
     rows = readCSV()
-    return jsonify(rows[-100:])
+    return jsonify(rows[-count:])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
