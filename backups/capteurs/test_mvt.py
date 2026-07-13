dab8cbe1e17f900fc5198e11501cc43f7bdda31c
hugo <hugoconstantin78@gmail.com>
2026-07-13 15:53:25 +0200

    test mvt

--- backups/capteurs/test_mvt.py
+++ backups/capteurs/test_mvt.py
@@ -185,7 +185,7 @@ while True:
     rover.brake()
     time.sleep(1.5)
     rover.spinLeft(speed)
-    time.sleep(2.56)
+    time.sleep(2.5)
     rover.brake()
     time.sleep(1.5)
     goForward(speed)
@@ -197,7 +197,7 @@ while True:
     rover.brake()
     time.sleep(1.5)
     rover.spinLeft(speed)
-    time.sleep(2.56)
+    time.sleep(2.5)
     rover.brake()
     time.sleep(1.5)
 
