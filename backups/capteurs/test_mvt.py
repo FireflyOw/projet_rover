80a50b004db4eb2b8b63b2b266d06dec46d21fb6
Julien <julien.marconcini@pm.me>
2026-07-13 15:19:32 +0200

    test mvt

--- backups/capteurs/test_mvt.py
+++ backups/capteurs/test_mvt.py
@@ -3,7 +3,7 @@ import time
 import sys, os, time
 
 dossier = os.path.dirname(__file__)
-racineProjet = os.path.abspath(os.path.join(dossier, "..", "marsRover"))
+racineProjet = os.path.abspath(os.path.join(dossier, "..", ".."))
 sys.path.append(racineProjet)
 
 import backups.marsRover.rover as rover
