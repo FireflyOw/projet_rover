974f63f5b09958396b95bf521989a368d44948bf
Julien <julien.marconcini@pm.me>
2026-07-13 16:42:55 +0200

    Add: bus i2c alternatif

--- backups/capteurs/capteurHM3301.py
+++ backups/capteurs/capteurHM3301.py
@@ -11,7 +11,10 @@ def mesure():
     pi.bb_i2c_zip(SDA, [4, 0x40, 2, 7, 1, 0x88, 3, 0])
     time.sleep(0.3)
 
-    data = pi.bb_i2c_zip(SDA, [4, 0x40, 2, 6, 29, 3, 0])
+    count, data = pi.bb_i2c_zip(SDA, [4, 0x40, 2, 6, 29, 3, 0])
+
+    if count < 29:
+        raise RuntimeError(f"Erreur: lecture incomplète, {count}/29 octets reçus!")
 
     pm1_atm = (data[10] << 8) | data[11]
     pm25_atm = (data[12] << 8) | data[13]
@@ -23,11 +26,15 @@ def mesure():
         "pm10_atm": pm10_atm,
     }
 
-while True:
-    valeur = mesure()
+try:
+    while True:
+        valeur = mesure()
 
-    print(f"""PM 1 : {valeur["pm1_atm"]} ug/m3
+        print(f"""PM 1 : {valeur["pm1_atm"]} ug/m3
 PM2.5 : {valeur["pm25_atm"]} ug/m3
 PM10 : {valeur["pm10_atm"]} ug/m3""")
-    
-    time.sleep(2)
+        
+        time.sleep(2)
+
+finally:
+    pi.bb_i2c_close(SDA)
