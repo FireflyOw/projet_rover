ea91e3493978990d90a678a01a5460db3ecda86e
Julien <julien.marconcini@pm.me>
2026-07-13 17:01:45 +0200

    Add: bus i2c alternatif

--- main.py
+++ main.py
@@ -30,6 +30,11 @@ SDA, SCL = 25, 5
 adresse = 0x40
 
 pi = pigpio.pi()
+try:
+    pi.bb_i2c_close(SDA)
+except pigpio.error:
+    pass
+
 pi.bb_i2c_open(SDA, SCL, 100000)
 time.sleep(0.3)
 print("[main.py] Bus I2C alternatif ouvert! SDA: 25, SCL: 05")
