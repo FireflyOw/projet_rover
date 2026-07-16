bc3a86edaa7c418c9c1777aa0f15f06357d20a21
hugo <hugoconstantin78@gmail.com>
2026-07-16 16:12:38 +0200

    test mvt

--- backups/deplacements/test_mvt.py
+++ backups/deplacements/test_mvt.py
@@ -329,10 +329,11 @@ try:
     while True:
         d = rover.getDistance()
         print(d)
-        if abs((distance_max - d) - ecart) <= tolerance:
+        if (distance_max - ecart) <= 100:
             print("babaaa")
             end =time.time()
             print(start-end)
+            break
 
 
         time.sleep(0.02)
