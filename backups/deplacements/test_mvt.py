74c37307de93614e0609704391301f73f5a4cfe9
hugo <hugoconstantin78@gmail.com>
2026-07-28 16:35:05 +0200

    test mvt

--- backups/deplacements/test_mvt.py
+++ backups/deplacements/test_mvt.py
@@ -338,7 +338,7 @@ def tourner(offsets, angle_cible=90, sens="droite", vitesse=50, seuilGyro=0.5, t
         dernier_temps = maintenant
  
         
-        angle_parcouru += abs(m["gz"]) * dt
+        angle_parcouru += abs(m["gx"]) * dt
 
 
         compteur += 1
